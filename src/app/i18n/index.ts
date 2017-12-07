2686db5e8d85dd091027bf7f3256881f17b11ce9
Chonlasith Jucksriporn <chonlasith@gmail.com>
2017-12-07 20:09:56 +0700

    Refactor export/import

---
+++ src/app/i18n/index.ts
@@ -0,0 +1,11 @@
+import { default as en_US } from './langs/en';
+import { default as th_TH } from './langs/th';
+import { default as zh_CN } from './langs/zh';
+
+const TRANSLATIONS = {
+  'en': en_US,
+  'th': th_TH,
+  'zh': zh_CN
+};
+
+export default TRANSLATIONS;
